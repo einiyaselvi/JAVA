ca5d629fae59e14cc319f25f548c8dafa54525ef
einiyaselvie <einiyaselvielavarasan@gmail.com>
2025-07-24 19:16:55 +0530

    add

---
+++ JAVA BASIC/comments.java
@@ -0,0 +1,27 @@
+// single and multiple command line arguments
+/* 
+class comments{
+    public static void main(String[] args){
+        System.out.println("Hello Einiya");
+    }
+}
+
+*/
+    
+
+//multipleline comment
+class comments{
+    public static void main(String[] args){
+        System.out.println("Hello Einiya");
+        /*
+        Let me know which topic you're focusing on first (like Inheritance, Exception Handling, etc.), and I can give you simple programs, syntax, and mini-tasks to learn hands-on.
+ */
+    }
+}
+ 
+
+/*  OUTPUT
+PS C:\Users\HP\OneDrive\Desktop\java\JAVA BASIC> javac .\comments.java
+PS C:\Users\HP\OneDrive\Desktop\java\JAVA BASIC> java comments        
+Hello Einiya
+*/
